93543ae00ea979ad5ce364b98aa9c114e1ef2479
a-sad-dan <danishasad91@gmail.com>
2025-03-12 15:54:25 +0530

    improved notification on linux, added logo, improved code formatting

--- bin/notify.sh
+++ bin/notify.sh
@@ -4,16 +4,22 @@ set -euo pipefail
 
 #notify-send "$1" # Linux desktop notification
 
-sendNotification(){
-  # 
+sendNotification() {
+  #
   message="${1:-Task Completed!}" # Use first argument or default message
 
   if command -v notify-send &>/dev/null; then
-    notify-send "$message" # Linux notification
+    notify-send -i "$(realpath ./../logo.png)" \
+      -t 2000 \
+      -a "Bashodoro" \
+      "Bashodoro" "$message"
+
   elif command -v osascript &>/dev/null; then
     osascript -e "display notification \"$message\" with title \"BASHodoro\""
+
   elif command -v powershell.exe &>/dev/null; then
     powershell.exe -Command "[System.Windows.Forms.MessageBox]::Show('$message')"
+
   else
     echo "No notification tool found. Message: $message"
   fi
@@ -23,7 +29,7 @@ case "$1" in
 start) sendNotification "Timer Started!" ;;
 pause) sendNotification "Timer Paused!" ;;
 resume) sendNotification "Timer Resumed!" ;;
-stop) sendNotification "Timer Stopped!" ;; 
+stop) sendNotification "Timer Stopped!" ;;
 complete) sendNotification "Time's up!" ;;
 *) echo "Usage: $0 {start|pause|resume|stop}" ;;
 esac
