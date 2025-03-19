447676bd0681df277338e99747a0487e3b8ef699
akram khan <akramkhan75533@gmail.com>
2025-03-19 16:44:39 +0530

    updated config of sounds and notifications

--- bin/notify.sh
+++ bin/notify.sh
@@ -2,38 +2,43 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 set -euo pipefail
+source "config/settings.conf"
 
 #notify-send "$1" # Linux desktop notification
 
 notificationSound() {
-  if command -v paplay &>/dev/null; then
-    paplay "$SCRIPT_DIR/sounds/$1" & # Linux (PulseAudio) -> Fixed Blocking of script
-  elif command -v afplay &>/dev/null; then
-    afplay "$SCRIPT_DIR/sounds/$1" & # macOS (Built-in)
-    #elif command -v powershell.exe &>/dev/null; then
-    #powershell.exe -c (New-Object Media.SoundPlayer 'sounds\"$1"').PlaySync()  # Windows
-  else
-    echo "No supported audio player found."
+  if [ "$SOUNDS" = true ]; then
+    if command -v paplay &>/dev/null; then
+      paplay "$SCRIPT_DIR/sounds/$1" & # Linux (PulseAudio) -> Fixed Blocking of script
+    elif command -v afplay &>/dev/null; then
+      afplay "$SCRIPT_DIR/sounds/$1" & # macOS (Built-in)
+      #elif command -v powershell.exe &>/dev/null; then
+      #powershell.exe -c (New-Object Media.SoundPlayer 'sounds\"$1"').PlaySync()  # Windows
+    else
+      echo "No supported audio player found."
+    fi
   fi
 }
 
 sendNotification() {
-  message="${1:-Task Completed!}" # Use first argument or default message
+  if [ "$NOTIFICATIONS" = true ]; then
+    message="${1:-Task Completed!}" # Use first argument or default message
 
-  if command -v notify-send &>/dev/null; then
-    notify-send -i "$SCRIPT_DIR/logo.png" \
-      -t 1000 \
-      -a "Bashodoro" \
-      "Bashodoro" "$message"
+    if command -v notify-send &>/dev/null; then
+      notify-send -i "$SCRIPT_DIR/logo.png" \
+        -t 1000 \
+        -a "Bashodoro" \
+        "Bashodoro" "$message"
 
-  elif command -v osascript &>/dev/null; then
-    osascript -e "display notification \"$message\" with title \"BASHodoro\""
+    elif command -v osascript &>/dev/null; then
+      osascript -e "display notification \"$message\" with title \"BASHodoro\""
 
-  elif command -v powershell.exe &>/dev/null; then
-    powershell.exe -Command "[System.Windows.Forms.MessageBox]::Show('$message')"
+    elif command -v powershell.exe &>/dev/null; then
+      powershell.exe -Command "[System.Windows.Forms.MessageBox]::Show('$message')"
 
-  else
-    echo "No notification tool found. Message: $message"
+    else
+      echo "No notification tool found. Message: $message"
+    fi
   fi
 }
 
